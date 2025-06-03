558ba58008aa8371aa9d8a0cfd9f6a03a64530a2
SeerDerawlier <35135507+SeerDerawlier@users.noreply.github.com>
2025-06-03 21:39:31 +0400

    Update br-rtr.sh

--- br-rtr.sh
+++ br-rtr.sh
@@ -14,7 +14,7 @@ connect port te0 service-instance te0/int0
 
 interface int1
 description "to br-srv"
-ip address 192.168.24.1/28
+ip address 192.168.23.1/28
 port te1
 service-instance te1/int1
 encapsulation untagged
@@ -35,7 +35,7 @@ exit
 router ospf 1
 router-id 2.2.2.2
 network 172.16.0.0/30 area 0
-network 192.168.24.0/28 area 0 
+network 192.168.23.0/28 area 0 
 passive-interface default
 no passive-interface tunnel.0
 exit
@@ -48,11 +48,11 @@ ip nat inside
 int int0
 ip nat outside
 exit
-ip nat pool NAT_POOL 192.168.24.1-192.168.24.14
+ip nat pool NAT_POOL 192.168.23.1-192.168.23.14
 ip nat source dynamic inside-to-outside pool NAT_POOL overload interface int0
 ntp timezone utc+4
-ip nat source static tcp 192.168.24.14 8086 172.16.50.14 80
-ip nat source static tcp 192.168.24.14 3015 172.16.50.14 3015
+ip nat source static tcp 192.168.23.14 8086 172.16.50.14 80
+ip nat source static tcp 192.168.23.14 3015 172.16.50.14 3015
 security none
 exit
 wr mem
